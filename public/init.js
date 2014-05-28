7246abdd4b674a6cb354d4d3ee9aae1067555d8b
Ioan Petras <jan.petras@yahoo.com>
2014-05-28 23:36:39 +0100

    from bootstrap to foundation

--- public/init.js
+++ public/init.js
@@ -17,7 +17,7 @@ for (var index in window.modules) {
 }
 
 // Default modules
-var modules = ['ngCookies', 'ngResource', 'ngAnimate', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.auth'];
+var modules = ['ngCookies', 'ngResource', 'ngAnimate', 'ui.router', 'mean.system', 'mean.auth', 'mm.foundation'];
 modules = modules.concat(packageModules);
 
 // Combined modules
--- public/system/controllers/index.js
+++ public/system/controllers/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'ImageSol', function ($scope, Global, ImageSol) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'ImageSol', function ($scope, Global, ImageSol, AlertDemoCtrl) {
     $scope.global = Global;
     $scope.logo = ImageSol.logo;
+      $scope.alerts = [
+    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'success round', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 }]);
