aa56f08a013006d8f6d582afd139e99c25c67d77
Ioan Petras <jan.petras@yahoo.com>
2014-05-30 18:02:10 +0100

    to foundation

---
+++ public/system/controllers/foundation.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('mean.system').controller('offCanvasCtrl',['$scope', function ($scope) {
+}]);
--- public/system/controllers/index.js
+++ public/system/controllers/index.js
@@ -1,18 +1,6 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'ImageSol', function ($scope, Global, ImageSol, AlertDemoCtrl) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'ImageSol', function ($scope, Global, ImageSol) {
     $scope.global = Global;
     $scope.logo = ImageSol.logo;
-      $scope.alerts = [
-    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
-    { type: 'success round', msg: 'Well done! You successfully read this important alert message.' }
-  ];
-
-  $scope.addAlert = function() {
-    $scope.alerts.push({msg: 'Another alert!'});
-  };
-
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
 }]);
