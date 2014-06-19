e54d8e658087cd255dd6ef18e416c08a3cf3031e
Ioan Petras <jan.petras@yahoo.com>
2014-06-19 18:01:27 +0100

    sidebar

--- public/system/system.js
+++ public/system/system.js
@@ -3,35 +3,10 @@
 angular.module('mean.system', ['mean.controllers.login','mean-factory-interceptor'])
 	.run(function($rootScope) {
     	$rootScope.lefty = function () {
-	
-	var body = document.body,
-		mask = document.createElement('div'),
-		activeNav
-	;
-	mask.className = 'mask';
-
-	/* slide menu left */
-	
-		$('body').addClass('sml-open');
-		document.body.appendChild(mask);
-		activeNav = 'sml-open';
-	
-
-};
+			$('body').toggleClass('sml-open');
+		};
+    	$rootScope.leftySleep = function () {
+			$('body').removeClass('sml-open');
+		};
 });
 
-		/* hide active menu if mask is clicked 
-	mask.addEventListener( 'click', function(){
-		classie.remove( body, activeNav );
-		activeNav = '';
-		document.body.removeChild(mask);
-	} );
-
-	/* hide active menu if close menu button is clicked 
-	[].slice.call(document.querySelectorAll('.close-menu')).forEach(function(el,i){
-		el.addEventListener( 'click', function(){
-			classie.remove( body, activeNav );
-			activeNav = '';
-			document.body.removeChild(mask);
-		} );
-	}); */
